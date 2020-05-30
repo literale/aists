f4c917847a5289718369fff5c46aa85c002729c1
literale <43133944+literale@users.noreply.github.com>
2020-05-30 20:46:14 +0500

    Доделан анализ клиентов и их закупок

--- АИСТ/Class/algoritms/algoritms.cs
+++ АИСТ/Class/algoritms/algoritms.cs
@@ -20,23 +20,13 @@ namespace АИСТ.Class.algoritms
             List<Customers> lc = gs.customers;
             DateTime[] activ = lc[0].Get_active();
             int[] av_sum = lc[0].Get_averrage_sum();
-            string[] shops = lc[0].Get_shops();
+            string[] shops = lc[0].Get_shops();////////////БЛИН ВВЕДИ ИХ В ОТБОР
             //КЛИЕНТЫ
-            string[]  clients = Get_Activ_client(activ).ToArray();
-            Timer t = new Timer();
-            t.Start();
-            Dictionary<string, double> cl_sum = Get_Clients_by_sum(clients, av_sum, activ);
-            t.Start();
-            Dictionary<string, double> cl_sum1 = Get_Clients_by_sum_and_active(av_sum, activ);
-            t.Stop();
-            List<Client_Tab> client_tabs = client_Analitic_ABC(cl_sum);
-            //client_tabs = client_Analitic_XYZ(client_tabs);
-
-
-
+            List<Client_Tab> client_tabs = Get_Clients_by_sum_and_active(av_sum, activ);
+            int o = 89;
         }
 
-        public Dictionary<string, double> Get_Clients_by_sum_and_active(int[] av_sum, DateTime[] active)
+        public List<Client_Tab> Get_Clients_by_sum_and_active(int[] av_sum, DateTime[] active)
         {
             Dictionary<string, double> cust = new Dictionary<string, double>();
             string table_name = "checks";
@@ -49,6 +39,7 @@ namespace АИСТ.Class.algoritms
             request = "SELECT * FROM checks WHERE check_date > \"" + s1 + "\" AND check_date < \"" + s2 + "\";";
             DataTable temp_dt = SQL_Helper.Just_do_it(request);
             Dictionary<string, double[]> client_sum = new Dictionary<string, double[]>();
+            Dictionary<string, List<string>> client_checks = new Dictionary<string, List<string>>();
             foreach (DataRow s in temp_dt.Rows)
             {
                 object[] temp = s.ItemArray;
@@ -59,83 +50,79 @@ namespace АИСТ.Class.algoritms
                     double[] sm = client_sum[id];
                     sm[0] += sum;
                     sm[1] += 1;
+                    client_checks[id].Add(temp[0].ToString());
                 }
                 else
                 {
                     client_sum.Add(id, new double[] { sum, 1 });
-                }    
-            }
-
+                    List<string> l = new List<string>();
+                    l.Add(temp[0].ToString());
+                    client_checks.Add(id, l);
+                }
+            }//собираем чеки
             foreach (string key in client_sum.Keys)
             {
                 double av = client_sum[key][0] / client_sum[key][1];
                 if (av > av_sum[0] && av <= av_sum[1])
                 {
                     cust.Add(key, client_sum[key][0]);
-                }
-            }
 
-            return cust;
-        }
-        public List<string> Get_Client_Base()
-        {
-            DataTable allCustomers = SQL_Helper.Try_To_Connect_Full("customers");
-            List<string> cust_Id = new List<string>();
-            foreach (DataRow s in allCustomers.Rows)
-            {
-                object[] temp = s.ItemArray;
-                cust_Id.Add(temp[0].ToString());
-            }
-            return cust_Id;
-        }
-        public List<string> Get_Activ_client( DateTime[] active)
-        {
-            List<string> activ_cust = new List<string>();
-            string table_name = "checks";
-            string request = "";
-            string s1 = active[0].ToString("u");
-            s1 = s1.Substring(0, 10);
-            string s2 = active[1].ToString("u");
-            s2 = s2.Substring(0, 10);
-            request = "SELECT * FROM checks WHERE check_date > \"" + s1 + "\" AND check_date < \"" + s2 + "\";";
-            DataTable temp_dt = SQL_Helper.Just_do_it(request);
-            foreach (DataRow s in temp_dt.Rows)
-            {
-                object[] temp = s.ItemArray;
-                activ_cust.Add(temp[3].ToString());
-            }
-            return activ_cust;
-        }
-        public Dictionary<string, double> Get_Clients_by_sum(string[] clients, int[] av_sum, DateTime[] active)
-        {
-            Dictionary<string, double> cust = new Dictionary<string, double>();
-            string table_name = "checks";
-            string request = "";
-            string s1 = active[0].ToString("u");
-            s1 = s1.Substring(0, 10);
-            string s2 = active[1].ToString("u");
-            s2 = s2.Substring(0, 10);
-            foreach (String client_id in clients)
-            {
-               
-                request = "SELECT * FROM checks WHERE ID_customer_check = " + client_id + " AND check_date > \"" + s1 + "\" AND check_date < \"" + s2 + "\";";
-                DataTable temp_dt = SQL_Helper.Just_do_it(request);
-                double sum = 0;
-                double count = 0;
-                foreach (DataRow s in temp_dt.Rows)
+                }
+                else
                 {
-                    object[] temp = s.ItemArray;
-                    sum += Convert.ToDouble(temp[4]);
-                    count++; 
+                    client_checks.Remove(key);
                 }
-                double av = sum / count;
-                if ((av > av_sum[0] && av <= av_sum[1])&&(cust.ContainsKey(client_id)==false))
-                { 
-                    cust.Add(client_id, sum);
+            }//выбираем клиентов с нужными суммами
+            List<Client_Tab> client_tabs = client_Analitic_ABC(cust);
+            foreach (Client_Tab ct in client_tabs)
+            {
+                Dictionary<string, int> checks_contains = new Dictionary<string, int>();
+                List<string> checks = client_checks[ct.Get_id()];
+                foreach (string c_id in checks)
+                {
+                    request = "SELECT * FROM history WHERE ID_check_history = '" + c_id + "';";
+                    temp_dt = SQL_Helper.Just_do_it(request);
+                    foreach (DataRow s in temp_dt.Rows)
+                    {
+                        string id_prod = s.ItemArray[1].ToString();
+
+                        if (checks_contains.ContainsKey(id_prod))
+                        {
+                            checks_contains[id_prod]+=1;
+
+                        }
+                        else
+                        {
+                            checks_contains.Add(id_prod, 1);
+                        }
+                    }
                 }
-            }
-            return cust;
+                checks_contains = checks_contains.OrderBy(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+                int count = checks_contains.Count();
+                double x = count * 0.75;
+                double y = count * 0.20;
+                double z = count * 0.05;
+                int i = 0;
+                foreach (string id in checks_contains.Keys)
+                {
+                    if (0 <= i && i <= z)
+                    {
+                        ct.Add_prod(id, enums.Type_XYZ.Z);
+                    }
+                    if (z < i && i <= y)
+                    {
+                        ct.Add_prod(id, enums.Type_XYZ.Y);
+                    }
+                    if (y < i)
+                    {
+                        ct.Add_prod(id, enums.Type_XYZ.X);
+                    }
+                    i++;
+                }
+            }//собираем и анализируем продукты
+            return client_tabs;
         }
+       
         public List<Client_Tab> client_Analitic_ABC(Dictionary<string, double> cl_sum)
         {
             List<Client_Tab> ct = new List<Client_Tab>();
@@ -175,19 +162,7 @@ namespace АИСТ.Class.algoritms
 
             return ct;
         }
-        public List<Client_Tab> client_Analitic_XYZ(List<Client_Tab> client_tabs)
-        {
-            foreach (Client_Tab ct in client_tabs)
-            {
-                string client_id = ct.Get_id();
-                string request = "SELECT * FROM checks WHERE ID_customer_check = \"" + client_id + "\";";
-                DataTable temp_dt = SQL_Helper.Just_do_it(request);
-                Dictionary<string, double> prod_count = new Dictionary<string, double>();
-
-            }
-
-            return client_tabs;
-        }
+       
 
     }
 }
